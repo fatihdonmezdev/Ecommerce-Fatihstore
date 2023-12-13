5d6ceb954fa8edc6c6488262ff46fa5388bb6f0c
Fatih <fdonmez_1512@hotmail.com>
2023-12-14 00:59:14 +0300

    Added link to the image.

--- src/components/footer/index.js
+++ src/components/footer/index.js
@@ -1,5 +1,4 @@
 import React from "react";
-import logo from "../../../public/FD-black.png";
 import facebook from "/public/assets/footer/facebook.svg";
 import instagram from "/public/assets/footer/instagram.svg";
 import twitter from "/public/assets/footer/twitter.svg";
@@ -8,7 +7,13 @@ import Image from "next/image";
 const Footer = () => {
   return (
     <div className="bg-black pb-4 pt-4 lg:px-10  flex justify-between ">
-      <Image alt="Hero" className="w-40 h-auto" src={logo} />
+      <Image
+        width={100}
+        height={100}
+        alt="Hero"
+        className="w-40 h-auto"
+        src="https://i.ibb.co/rvfytMz/FD-black.png"
+      />
       <p className="text-white hidden">
         @2023 Fatih Donmez. All Rights are reserved
       </p>
