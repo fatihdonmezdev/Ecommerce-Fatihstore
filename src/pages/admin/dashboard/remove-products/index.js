67d92d64b2bbe4f3d6f8833e1f5ac5c3b4478f1a
Fatih <fdonmez_1512@hotmail.com>
2023-12-14 18:00:45 +0300

    Refactored the code in the admin dashboard.

--- src/pages/admin/dashboard/remove-products/index.js
+++ src/pages/admin/dashboard/remove-products/index.js
@@ -1,3 +1,4 @@
+import ProductsTable from "@/components/admin/ProductsTable";
 import Sidebar from "@/components/admin/Sidebar";
 import { db } from "@/pages/_app";
 import { fetchProducts } from "@/store/productSlice";
@@ -26,52 +27,11 @@ const RemoveProduct = () => {
   return (
     <div>
       <ToastContainer />
-      <div className="ml-80 mt-40 relative overflow-x-auto shadow-md sm:rounded-lg">
-        <table className="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
-          <thead className="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
-            <tr>
-              <th scope="col" className="px-6 py-3">
-                Product name
-              </th>
-              <th scope="col" className="px-6 py-3">
-                Category
-              </th>
-              <th scope="col" className="px-6 py-3">
-                Price
-              </th>
-              <th scope="col" className="px-6 py-3">
-                Action
-              </th>
-            </tr>
-          </thead>
-          <tbody>
-            {products.map((product) => (
-              <tr
-                key={product.name}
-                className="odd:bg-white odd:dark:bg-gray-900 even:bg-gray-50 even:dark:bg-gray-800 border-b dark:border-gray-700"
-              >
-                <th
-                  scope="row"
-                  className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white"
-                >
-                  {product.name}
-                </th>
-                <td className="px-6 py-4">{product.category}</td>
-                <td className="px-6 py-4">{product.price}</td>
-                <td className="px-6 py-4">
-                  <button
-                    type="button"
-                    onClick={() => removeProduct(product.id)}
-                    className="font-medium text-blue-600 dark:text-blue-500 hover:underline"
-                  >
-                    Remove
-                  </button>
-                </td>
-              </tr>
-            ))}
-          </tbody>
-        </table>
-      </div>
+      <ProductsTable
+        products={products}
+        action={removeProduct}
+        actionType={"Remove"}
+      />
     </div>
   );
 };
